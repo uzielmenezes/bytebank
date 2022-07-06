3c94afcec3ef9f31f5a7ada79bce1fc8e85247a6
uzielmenezes <uzielcivil@gmail.com>
2022-07-06 19:31:06 -0300

    displaying valor/destino object on browser

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -16,5 +16,11 @@ export class NovaTransferenciaComponent {
     console.log('Solicitada nova transferência!');
     const valorEmitir = { valor: this.valor, destino: this.destino };
     this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
